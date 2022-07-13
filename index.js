a5600dc8ad331c3677a5c7211ed82e31a05a204a
stavbers <reddevweb@gmail.com>
2022-07-13 18:58:32 +0300

    add new fns

--- index.js
+++ index.js
@@ -1,35 +1,48 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
+  const app = document.querySelector('#App')
 
-const searchTitle = document.querySelector('.app-header__title')
-const searchInp = document.querySelector('.app-header__input')
-const searchBtn = document.querySelector('.app-header__btn')
+  const baseUrl = 'https://api.github.com/users'
+  const searchTitle = document.querySelector('.app-header__title')
+  const searchInp = document.querySelector('.app-header__input')
+  const searchBtn = document.querySelector('.app-header__btn')
+  let inpDate = ''
 
+  searchBtn.addEventListener('click', e => {
+    searchTitle.classList.toggle('hideEl')
+    searchBtn.classList.toggle('act')
+    if (searchInp.classList.contains('hideEl')) {
+      searchInp.classList.remove('hideEl')
+      searchInp.classList.add('inpWidth')
+    } else {
+      searchInp.classList.add('hideEl')
+      searchInp.classList.remove('act')
+    }
+  })
 
-searchBtn.addEventListener('click', e => {
-  searchTitle.classList.toggle('hideEl')
-  if(searchInp.classList.contains('hideEl')){
-    searchInp.classList.remove('hideEl')
-    searchInp.classList.add('inpWidth')
-  } else {
-    searchInp.classList.add('hideEl')
-    searchInp.classList.remove('inpWidth')
+  searchBtn.addEventListener('click', () => {
+    if (searchBtn.classList.contains('act')) {
+      if (searchInp.value.length > 3) {
+        inpDate = searchInp.value
+        init(inpDate)
+        searchInp.value = ''
+      }
+    }
+  })
+
+  function init(value) {
+    const url = `https://api.github.com/search/users?q=${value}`
+    const a = getUserName(url)
+    a.then(e => createItem(e))
   }
-})
 
-
-
-
-
-
-const url = 'https://api.github.com/search/users?q=andrew'
-const baseUrl = 'https://api.github.com/users'
-// const a = getUserName(url)
-// a.then(e => console.log(e))
+  function createItem(data) {
+    console.log(data)
+  }
 
 })
 
- const getUserName = async(url) => {
- const resp = await fetch(url)
- const data = await resp.json()
- return await data.items[0]
+const getUserName = async (url) => {
+  const resp = await fetch(url)
+  const data = await resp.json()
+  return await data.items[0]
 }
