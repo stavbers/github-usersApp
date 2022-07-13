71064062b9462826a46ff09e0be75b7132c90efb
stavbers <reddevweb@gmail.com>
2022-07-13 03:08:03 +0300

    add fn js and style header search

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
+
+const searchTitle = document.querySelector('.app-header__title')
+const searchInp = document.querySelector('.app-header__input')
+const searchBtn = document.querySelector('.app-header__btn')
+
+
+searchBtn.addEventListener('click', e => {
+  searchTitle.classList.toggle('hideEl')
+  searchInp.classList.toggle('hideEl')
+  
+})
+
+
+
+
+
+
 const url = 'https://api.github.com/search/users?q=freeCodeCamp'
-
-
 const a = getUserName(url)
-
 a.then(e => console.log(e))
 
 })
